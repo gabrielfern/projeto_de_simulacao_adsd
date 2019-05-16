d8815703bf5f82fba09575cfd37abc817c43f2c0
Gabriel Fernandes <gabrielfernnd@gmail.com>
2019-05-15 23:43:41 -0300

    add ClientWebBrowser functional example

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from random import randint
 import simpy
 
+
 class CPU:
     def __init__(self, env):
         self.env = env
         self.disk = Disk(env)
-        self.action = env.process(self.run())
 
     def run(self):
-        while True:
-            print('CPU running at %d' %env.now)
-            yield self.env.timeout(randint(1, 3))
-            print('waiting the Disk at %d' %env.now)
-            yield self.env.process(self.disk.get_resource())
+        print('CPU running at %d' %env.now)
+        yield self.env.timeout(randint(1, 3))
+        print('waiting the Disk at %d' %env.now)
+        yield self.env.process(self.disk.get_resource())
 
 
 class Disk:
@@ -23,6 +22,23 @@ class Disk:
         yield self.env.timeout(randint(5, 10))
 
 
+class ClientWebBrowser:
+    def __init__(self, env):
+        self.env = env
+        self.cpu = CPU(env)
+        self.action = env.process(self.working())
+
+    def client_thinking(self):
+        yield self.env.timeout(randint(20, 100))
+
+    def working(self):
+        while True:
+            yield self.env.process(self.client_thinking())
+            print('Client acts at %d' %env.now)
+            if env.now % 2 == 0:
+                yield self.env.process(self.cpu.run())
+
+
 env = simpy.Environment()
-CPU(env)
-env.run(until=30)
+ClientWebBrowser(env)
+env.run(until=300)
