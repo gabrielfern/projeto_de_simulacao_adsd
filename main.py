e44fbf56bb61e169a0bdb6542a7943392a7ca590
Gabriel Fernandes <gabrielfernnd@gmail.com>
2019-06-03 17:05:34 -0300

    busy time
    possible to set a different time range of work to a process

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ DURATION = 100
 
 
 class CPU:
-    def __init__(self, env, id, next_server):
+    def __init__(self, env, id, next_server, busy_time=(1, 3)):
         self.env = env
         self.disk = Disk(env, id)
         self.id = id
         self.next_server = next_server
+        self.busy_time = busy_time
         self.core = simpy.Resource(env, capacity=1)
 
     def process(self, client):
         with self.core.request() as req:
             yield req
-            rand = randint(1, 3)
+            rand = randint(self.busy_time[0], self.busy_time[1])
             print('%s - CPU running from %d" to %d" for %s' %(self.id,
                 env.now, env.now + rand, client))
             yield self.env.timeout(rand)
@@ -36,29 +37,31 @@ class CPU:
 
 
 class Disk:
-    def __init__(self, env, id):
+    def __init__(self, env, id, busy_time=(5, 10)):
         self.env = env
         self.id = id
+        self.busy_time = busy_time
         self.controller = simpy.Resource(env, capacity=1)
 
     def get_resource(self, client):
         with self.controller.request() as req:
             yield req
-            rand = randint(5, 10)
+            rand = randint(self.busy_time[0], self.busy_time[1])
             print('%s - Disk working from %d" to %d" for %s' %(self.id,
                 env.now, env.now + rand, client))
             yield self.env.timeout(rand)
 
 
 class ClientWebBrowser:
-    def __init__(self, env, server, id):
+    def __init__(self, env, server, id, busy_time=(20, 100)):
         self.env = env
         self.server = server
         self.id = id
+        self.busy_time = busy_time
         self.action = env.process(self.working())
 
     def client_thinking(self):
-        yield self.env.timeout(randint(20, 100))
+        yield self.env.timeout(randint(self.busy_time[0], self.busy_time[1]))
 
     def working(self):
         while True:
