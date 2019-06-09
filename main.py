a99d701f4c44dab5f4577e0c8b013842c9f99cb6
Gabriel Fernandes <gabrielfernnd@gmail.com>
2019-06-08 22:13:40 -0300

    ClientWebBrowser queue
    Client can now make another action before the last one be fully handled

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sys
 import simpy
 
 CLIENTS = 5
-DURATION = 100
+DURATION = 500
 
 
 class CPU:
@@ -81,23 +81,52 @@ class Disk:
 
 
 class ClientWebBrowser:
-    def __init__(self, env, server, id, busy_time=(20, 100), server_prob=0.5):
+    usage_time = [0 for _ in range(CLIENTS)]
+    response_time = [[] for _ in range(CLIENTS)]
+    wait_time = [[] for _ in range(CLIENTS)]
+    queue_size = [[] for _ in range(CLIENTS)]
+
+    def __init__(self, env, server, id, index, client_time=(10, 100), busy_time=(1, 3), server_prob=0.5):
         self.env = env
         self.server = server
         self.id = id
+        self.index = index
+        self.client_time = client_time
         self.busy_time = busy_time
         self.server_prob = server_prob
-        self.action = env.process(self.working())
+        self.thread = simpy.Resource(env, capacity=1)
+        env.process(self.client_loop())
+
+    def process(self):
+        with self.thread.request() as req:
+            ClientWebBrowser.queue_size[self.index].append(len(self.thread.queue))
+            arrival_time = self.env.now
+            yield req
+            ClientWebBrowser.wait_time[self.index].append(env.now - arrival_time)
+
+            rand = randint(self.busy_time[0], self.busy_time[1])
+            ClientWebBrowser.usage_time[self.index] += min(rand, DURATION - 1 - env.now)
+            print('%s Web Browser running from %d" to %d"' %(self.id,
+                    env.now, env.now + rand))
+
+            yield self.env.timeout(rand)
+            ClientWebBrowser.response_time[self.index].append(env.now - arrival_time)
+
+    def client_action_handler(self):
+        yield self.env.process(self.process())
+
+        while decision(self.server_prob):
+            yield self.env.process(self.server.run(self.id))
+            yield self.env.process(self.process())
 
     def client_thinking(self):
-        yield self.env.timeout(randint(self.busy_time[0], self.busy_time[1]))
+        yield self.env.timeout(randint(self.client_time[0], self.client_time[1]))
 
-    def working(self):
+    def client_loop(self):
         while True:
             yield self.env.process(self.client_thinking())
             print(self.id + ' acts at %d"' %env.now)
-            if decision(self.server_prob):
-                yield self.env.process(self.server.run(self.id))
+            self.env.process(self.client_action_handler())
 
 
 def decision(prob):
@@ -110,8 +139,9 @@ database_server = CPU(env, 'Database Server')
 application_server = CPU(env, 'Application Server', database_server)
 web_server = CPU(env, 'Web Server', application_server)
 
+clients = []
 for i in range(1, CLIENTS + 1):
-    ClientWebBrowser(env, web_server, 'Client ' + str(i))
+    clients.append(ClientWebBrowser(env, web_server, 'Client ' + str(i), i - 1))
 
 stdout = sys.stdout
 sys.stdout = open('output.log', 'w')
@@ -153,3 +183,25 @@ for server in (web_server, application_server, database_server):
     m = '%14.6f' %mean(server.disk.queue_size) if len(server.disk.queue_size) > 0 else 'NaN'.rjust(14)
     sd = '%11.6f' %stdev(server.disk.queue_size) if len(server.disk.queue_size) > 1 else 'NaN'.rjust(6)
     print('Queue Size mean: '+ m + '  std deviation: ' + sd)
+
+
+print(('Client Web Browsers').ljust(59, '-'))
+utilization = [usage_time / DURATION for usage_time in ClientWebBrowser.usage_time]
+m = '%13.6f' %mean(utilization) if len(utilization) > 0 else  'NaN'.rjust(13)
+sd = '%11.6f' %stdev(utilization) if len(utilization) > 1 else 'NaN'.rjust(6)
+print('Utilization mean: ' + m + '  std deviation: ' + sd)
+
+response_time = [item for sublist in ClientWebBrowser.response_time for item in sublist]
+m = '%11.6f' %mean(response_time) if len(response_time) > 0 else  'NaN'.rjust(11)
+sd = '%11.6f' %stdev(response_time) if len(response_time) > 1 else 'NaN'.rjust(6)
+print('Response Time mean: ' + m + '  std deviation: ' + sd)
+
+wait_time = [item for sublist in ClientWebBrowser.wait_time for item in sublist]
+m = '%15.6f' %mean(wait_time) if len(wait_time) > 0 else 'NaN'.rjust(15)
+sd = '%11.6f' %stdev(wait_time) if len(wait_time) > 1 else 'NaN'.rjust(6)
+print('Wait Time mean: ' + m + '  std deviation: ' + sd)
+
+queue_size = [item for sublist in ClientWebBrowser.queue_size for item in sublist]
+m = '%14.6f' %mean(queue_size) if len(queue_size) > 0 else 'NaN'.rjust(14)
+sd = '%11.6f' %stdev(queue_size) if len(queue_size) > 1 else 'NaN'.rjust(6)
+print('Queue Size mean: '+ m + '  std deviation: ' + sd)
